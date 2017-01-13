3c4d366d9ac991b2313c224b801623584691aeac
Bit <bitbytenetwork@gmail.com>
2017-01-12 19:13:28 -0700

    Update bot.js

--- scr/bot.js
+++ scr/bot.js
@@ -27,6 +27,12 @@ function botInit() {
         message.channel.send(processCommand(message.content).params.join(me.seperator));
       }
     },
+    ping: {
+      name: "ping",
+      runtime: function(message, client) {
+        message.channel.sendMessage("Pong!");
+      }
+    },
     play: {
       name: "play",
       runtime: function(message, client) {
@@ -36,7 +42,7 @@ function botInit() {
           //connection.playFile("./test.mp3", {});
           var stream = lib_ytdl(processCommand(message.content).params[0] || "https://www.youtube.com/watch?v=92f3RRkakO8", {filter: "audioonly"});
           setTimeout(function(stream) {
-            connection.playStream(stream, {});
+            connection.playStream(stream, {seek: 0, volume: 1});
           }, 2500, stream);
         });
       }
@@ -49,27 +55,28 @@ var client = new lib_discord.Client();
 
 client.on("ready", function() {
   console.log("Server ready!");
-  client.user.setGame("A Game");
+  client.user.setGame("Discord");
 });
 
 
-client.on("message", message => {
+client.on("message", function(message) {
   
   // Logging messages for debugging
   var header = message.channel.guild.name + "/" + message.channel.name + ", " + message.author.username + "#" + message.author.discriminator + ": ";
   console.log(header + message.content.replace(/\n/g, "\n" + " ".repeat(header.length)));
   
-  if (message.content.substring(0, bot.prefix.length) !== bot.prefix || message.author.id === client.user.id) { // NEED TO DO
+  if (message.content.substring(0, bot.prefix.length) !== bot.prefix || message.author.id === client.user.id) {
     return;
   }
   
   var command = message.content.substring(bot.prefix.length).split(bot.seperator)[0];
   var params = message.content.split(bot.seperator).slice(1);
     
-  // This is lazy af. Crashes when you call an undefined
-  // command. Use some sort of "loop through every .name
-  // value until it matches.
-  bot.commands[command].runtime(message, client);
+  for (var key in bot.commands) {
+    if (bot.commands[key].name === command && typeof bot.commands[key].runtime === "function") {
+      bot.commands[command].runtime(message, client);
+    }
+  }
 
 });
 
