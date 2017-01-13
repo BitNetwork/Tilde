2e8a27443cdeb9addd0266b6595ebe44e1627c5b
Bit <bitbytenetwork@gmail.com>
2017-01-13 08:20:20 -0700

    Update bot.js

--- scr/bot.js
+++ scr/bot.js
@@ -1,6 +1,5 @@
 const lib_discord = require("discord.js");
 const lib_ytdl = require("ytdl-core");
-const lib_google = require("google");
 
 function botInit() {
   var me = this;
@@ -50,94 +49,84 @@ function botInit() {
     },
     join: {
       name: "join",
-      runtime: function(message, client) {
+      runtime: function(message, client, data) {
         if (typeof message.member.voiceChannel === "undefined") {
           message.channel.sendMessage(makeMention(message.author.id) + " You don't appear to be in a voice channel.");
           return;
         }
         message.member.voiceChannel.join().then(function(connection) {
-          bot.guild[message.member.guild.id].voiceChannel = connection;
+          data.bin.voiceChannel = connection;
         });
       }
     },
     play: {
       name: "play",
-      runtime: function(message, client) {
+      runtime: function(message, client, data) {
+        if (typeof data.bin.voiceDispatcher !== "undefined") {
+          data.bin.voiceDispatcher.resume();
+          return;
+        }
+        
+        if (processCommand(message.content).params.length === 0) {
+          message.channel.sendMessage("```" + me.prefix + "play [yt video]\n\nPlays a song in your connected voice channel.```");
+          return;
+        }
+        
         if (typeof message.member.voiceChannel === "undefined") {
           message.channel.sendMessage(makeMention(message.author.id) + " You don't appear to be in a voice channel.");
           return;
         }
-        if (typeof bot.guild[message.member.guild.id].voiceDispatcher !== "undefined") {
-          bot.guild[message.member.guild.id].voiceDispatcher.resume();
-          return;
-        }
+        
         message.member.voiceChannel.join().then(function(connection) {
           //Get info with http://stackoverflow.com/q/38810536/3434588
-          bot.guild[message.member.guild.id].voiceChannel = connection;
+          data.bin.voiceChannel = connection;
           
           var url = "https://www.youtube.com/watch?v=92f3RRkakO8";
           var youtubeUrlRegex = /^(?:http(?:s)?:\/\/)?(?:www\.)?(?:(?:youtube.com(?:(?::80)|(?::443))?\/watch\?v=)|(?:youtu.be(?:(?::80)|(?::443))?\/))([\w\d]{8,13})/;
           if (processCommand(message.content).params.length > 0 && processCommand(message.content).params[0].match(youtubeUrlRegex) !== null && processCommand(message.content).params[0].match(youtubeUrlRegex).length > 1) {
             url = "https://www.youtube.com/watch?v=" + processCommand(message.content).params[0].match(youtubeUrlRegex)[1];
-            openAndStartStream();
-          } else if (processCommand(message.content).params.length > 0) {
-            lib_google.resultsPerPage = 25;
-            lib_google(processCommand(message.content).params.join(bot.seperator), function(error, response) {
-              if (error) {
-                message.channel.sendMessage("Could not retrive audio stream!");
-                return;
-              }
-              for (var i=0; i<response.links.length; i++) {
-                var link = response.links[i];
-                console.log(link); // Not getting offical songs
-                if (link.href !== null && link.href.match(youtubeUrlRegex) !== null && link.href.match(youtubeUrlRegex).length > 1) {
-                  url = "https://www.youtube.com/watch?v=" + link.href.match(youtubeUrlRegex)[1];
-                  openAndStartStream();
-                  break;
-                }
-              }
-            });
           }
           
-          function openAndStartStream() {
-            var stream = lib_ytdl(url, {filter: "audioonly"});
-            setTimeout(function(stream) {
-              bot.guild[message.member.guild.id].voiceDispatcher = connection.playStream(stream, {seek: 0, volume: 1});
-              bot.guild[message.member.guild.id].voiceDispatcher.on("end", function() {
-                bot.guild[message.member.guild.id].voiceDispatcher = undefined;
-              });
-            }, 2500, stream);
-          }
+          var stream = lib_ytdl(url, {filter: "audioonly"});
+          setTimeout(function(stream) {
+            data.bin.voiceDispatcher = connection.playStream(stream, {seek: 0, volume: 1});
+            data.bin.voiceDispatcher.on("end", function() {
+              data.bin.voiceDispatcher = undefined;
+            });
+          }, 2500, stream);
         });
       }
     },
     pause: {
       name: "pause",
-      runtime: function(message, client) {
-        if (typeof bot.guild[message.member.guild.id].voiceDispatcher === "undefined") {
+      runtime: function(message, client, data) {
+        if (typeof data.bin.voiceDispatcher === "undefined") {
+          message.channel.sendMessage("There's no music playing.");
           return;
         }
-        bot.guild[message.member.guild.id].voiceDispatcher.pause();
+        data.bin.voiceDispatcher.pause();
       }
     },
     stop: {
       name: "stop",
-      runtime: function(message, client) {
-        if (typeof bot.guild[message.member.guild.id].voiceDispatcher === "undefined") {
+      runtime: function(message, client, data) {
+        if (typeof data.bin.voiceDispatcher === "undefined") {
+          message.channel.sendMessage("There's no music playing.");
           return;
         }
-        bot.guild[message.member.guild.id].voiceDispatcher.end();
-        bot.guild[message.member.guild.id].voiceDispatcher = undefined;
+        data.bin.voiceDispatcher.end();
+        data.bin.voiceDispatcher = undefined;
       }
     },
     leave: {
       name: "leave",
-      runtime: function(message, client) {
-        if (typeof bot.guild[message.member.guild.id].voiceChannel === "undefined") {
+      runtime: function(message, client, data) {
+        if (typeof data.bin.voiceChannel === "undefined") {
+          message.channel.sendMessage("I'm not in a voice channel.");
           return;
         }
-        bot.guild[message.member.guild.id].voiceChannel.disconnect();
-        bot.guild[message.member.guild.id].voiceChannel = undefined;
+        data.bin.voiceChannel.disconnect();
+        data.bin.voiceChannel = undefined;
       }
     }
   };
@@ -155,15 +144,18 @@ client.on("ready", function() {
 client.on("message", function(message) {
   
   // Logging messages for debugging
-  var header = message.channel.guild.name + "/" + message.channel.name + ", " + message.author.username + "#" + message.author.discriminator + ": ";
+  var header = (typeof message.channel.guild === "object" ? message.channel.guild.name : "DM") + "/" + message.channel.name + ", " + message.author.username + "#" + message.author.discriminator + ": ";
   console.log(header + message.content.replace(/\n/g, "\n" + " ".repeat(header.length)));
   
-  if (message.content.substring(0, bot.prefix.length) !== bot.prefix || message.author.id === client.user.id) {
+  if (message.content.substring(0, bot.prefix.length) !== bot.prefix || message.author.id === client.user.id || typeof message.channel.guild !== "object") {
     return;
   }
   
-  if (typeof bot.guild[message.member.guild.id] === "undefined") {
-    bot.guild[message.member.guild.id] = {};
+  if (typeof bot.guild[message.channel.guild.id] === "undefined") {
+    bot.guild[message.channel.guild.id] = {
+      bin: {},
+      data: {}
+    };
   }
   
   var command = message.content.substring(bot.prefix.length).split(bot.seperator)[0];
@@ -171,7 +163,7 @@ client.on("message", function(message) {
     
   for (var key in bot.commands) {
     if (bot.commands[key].name === command && typeof bot.commands[key].runtime === "function") {
-      bot.commands[command].runtime(message, client);
+      bot.commands[command].runtime(message, client, bot.guild[message.channel.guild.id]);
     }
   }
 
