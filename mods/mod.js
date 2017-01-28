5be00ce084f7e0b0a5b34368f80685d42bd925fa
Bit <bitbytenetwork@gmail.com>
2017-01-27 23:29:45 -0700

    Permission & voiceclone commands added

--- mods/mod.js
+++ mods/mod.js
@@ -1,4 +1,14 @@
 {
+  game: {
+    name: "game",
+    runtime: function(message, client) {
+      if (processCommand(message.content).params.length === 0) {
+        message.channel.sendMessage("```" + me.prefix + "game [game]\n\nChanges the current game for this bot.```");
+        return;
+      }
+      client.user.setGame(processCommand(message.content).params.join(me.seperator));
+    }
+  },
   pin: {
     name: "pin",
     runtime: function(message, client) {
@@ -84,6 +94,35 @@
       });
     }
   },
+  voiceclone: {
+    name: "voiceclone",
+    runtime: function(message, client) {
+      var command = processCommand(message.content);
+      if (command.params.length < 1) {
+        message.channel.sendMessage("```" + me.prefix + "voiceclone [name]\n\nClones the current connected voice channel into a new channel.```");
+        return;
+      }
+
+      if (typeof message.member.voiceChannel === "undefined") {
+        message.channel.sendMessage(makeMention(message.author.id) + " You don't appear to be in a voice channel.");
+        return;
+      }
+
+      if (command.params[0].length > 100) {
+        message.channel.sendMessage("That name is too long.");
+        return;
+      }
+
+      if (command.params[0].length < 2) {
+        message.channel.sendMessage("That name is too short.");
+        return;
+      }
+
+      message.member.voiceChannel.clone(command.params[0]).then(function() {
+        message.channel.sendMessage("Voice channel cloned.");
+      });
+    }
+  },
   clear: {
     name: "clear",
     runtime: function(message, client) {
@@ -106,6 +145,45 @@
       message.channel.bulkDelete(amount).catch(function(error) {
         message.channel.sendMessage("I had some trouble deleting those messages.");
       });
+    }
+  },
+  permission: {
+    name: "permission",
+    startup: function(client, data) {
+      data.bin.perm = {};
+    },
+    runtime: function(message, client, data) {
+      var command = processCommand(message.content);
+      if (command.params.length < 2) {
+        message.channel.sendMessage("```" + me.prefix + "permission [command] [role]\n\nAllows or blocks the usage of a command by role.```");
+        return;
+      }
+
+      var target = findCommand(command.params[0]);
+      if (target === null) {
+        message.channel.sendMessage("Command not found.");
+        return;
+      }
+
+      //var role = message.guild.roles.find("name", command.params[1]);
+      var role = message.guild.roles.get(parseRoleMention(command.params[1]));
+      console.log(role);
+      if (typeof role === "undefined") {
+        message.channel.sendMessage("Role not found.");
+        return;
+      }
+
+      data.bin.perm[target.name] = role.id;
+      message.channel.sendMessage("Permission status updated.");
+    },
+    commandvalidate: function(message, client, data) {
+      var command = processCommand(message.content);
+      if (typeof data.bin.perm[command.command] !== "string") {
+        return true;
+      } else if (message.member.highestRole.comparePositionTo(message.guild.roles.get(data.bin.perm[command.command])) >= 0) {
+        return true;
+      }
+      return false;
     }
   },
   kick: {
