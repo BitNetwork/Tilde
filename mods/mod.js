02a7258e06a938f08e16cf07e0f39296e09bcd51
Bit <bitbytenetwork@gmail.com>
2017-02-04 16:00:12 -0700

    Update mod.js

--- mods/mod.js
+++ mods/mod.js
@@ -152,47 +152,6 @@
       });
     }
   },
-  permission: {
-    name: "permission",
-    startup: function(client, data) {
-      data.bin.perm = {};
-    },
-    runtime: function(message, client, data) {
-      var command = processCommand(message.content);
-      if (command.params.length < 2) {
-        message.channel.sendMessage("```" + me.prefix + "permission [command] [role]\n\nAllows or blocks the usage of a command by role.```");
-        return;
-      }
-
-      var target = findCommand(command.params[0]);
-      if (target === null) {
-        message.channel.sendMessage("Command not found.");
-        return;
-      }
-
-      var role = message.guild.roles.get(parseRoleMention(command.params[1]));
-      if (typeof role === "undefined") {
-        if (command.params[1] === "@everyone") {
-          role = message.guild.roles.find("name", command.params[1]);
-        } else {
-          message.channel.sendMessage("Role not found.");
-          return;
-        }
-      }
-
-      data.bin.perm[target.name] = role.id;
-      message.channel.sendMessage("Permission status updated.");
-    },
-    commandvalidate: function(message, client, data) {
-      var command = processCommand(message.content);
-      if (typeof data.bin.perm[command.command] !== "string") {
-        return true;
-      } else if (message.member.highestRole.comparePositionTo(message.guild.roles.get(data.bin.perm[command.command])) >= 0) {
-        return true;
-      }
-      return false;
-    }
-  },
   kick: {
     name: "kick",
     runtime: function(message, client) {
