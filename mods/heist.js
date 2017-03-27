769212ecef99af99dba246223e89ebfe67e7142c
Bit <BitNetwork@users.noreply.github.com>
2017-03-27 15:42:51 -0600

    Update heist.js

--- mods/heist.js
+++ mods/heist.js
@@ -3,10 +3,13 @@
     name: "heist",
     startup: function(client, data) {
       data.bin.heistCrew = [];
+      data.bin.heistLooters = [];
+      data.bin.heistJoinMessage = null;
       data.bin.heistState = 0; // 0: not running, 1: waiting for crew, 2: playing
+
       data.data.heistBanks = [{name: "Some Guy's Wallet", size: 2, money: 350, difficulty: 0.6}, {name: "ATM Machine", size: 5, money: 1000, difficulty: 0.4}];
       data.data.heistActions = [{text: "$name accidently tripped the alarm.", result: 2}, {text: "$name was shot by local security... but survived.", result: 3}, {text: "$name cut through that safe like butter.", result: 0}, {text: "$name slept in and completely forgot about the heist.", result: 1}, {text: "In the process of setting up the explosive, $name was explosively killed.", result: 4}]; // 0: win, 1: alive, 2: caught, 3: injured, 4: dead
-      data.data.heistConfig = {jailTimer: 5 * 1000 * 60, deathTimer: 20 * 1000 * 60};
+      data.data.heistConfig = {joinTimer: 20 * 1000, jailTimer: 5 * 1000 * 60, deathTimer: 20 * 1000 * 60};
     },
     runtime: function(message, client, data) {
       function help() {
@@ -32,9 +35,16 @@
         data.data.user[message.author.id].money = data.data.user[message.author.id].money ? data.data.user[message.author.id].money : 0;
         data.data.user[message.author.id].heistStatus = 1;
         data.data.user[message.author.id].heistTimer = 0;
+
+        data.data.user[message.author.id].heistStatWon = [0, 0]; // [total, this lifetime]
+        data.data.user[message.author.id].heistStatCaught = [0, 0];
+        data.data.user[message.author.id].heistStatInjured = [0, 0];
+        data.data.user[message.author.id].heistStatDead = [0];
+        data.data.user[message.author.id].heistStatStolden = 0;
+
         message.channel.sendMessage("Welcome to the crew, " + (message.member.nickname !==null ? message.member.nickname : message.author.username) + ".");
       } else if (command.params[0] === "stats") {
-        if (typeof data.data.user[message.author.id].heistStatus === "undefined") {
+        if (typeof data.data.user[message.author.id] === "undefined" || typeof data.data.user[message.author.id].heistStatus === "undefined") {
           message.channel.sendMessage("You're not part of the crew yet. Try joining them first.");
           return;
         }
@@ -46,29 +56,46 @@
         var status = "";
         var color = "";
         if (heister.heistStatus === 1) {
-          status = "Alive";
           color = "#00ff00"
         } else if (heister.heistStatus === 2) {
-          status = "Apprehended";
           color = "#0000ff";
         } else if (heister.heistStatus === 3) {
-          status = "Injured";
           color = "#ff0000"
         } else if (heister.heistStatus === 4) {
-          status = "Dead";
           color = "#000000"
         }
-        embed.setTitle(status);
         embed.setColor(color);
         // console.log(message.author.avatarURL)
         if (message.author.avatarURL !== null) {
           embed.setThumbnail(message.author.avatarURL);
         }
-        embed.addField("Timer", heister.heistTimer, true);
+
+        if (heister.heistStatus === 2 || heister.heistStatus === 4) {
+          var remaining = new Date(heister.heistTimer - Date.now());
+          var output = "";
+
+          if (remaining.getHours() !== 0) {
+            output += remaining.getHours() + " hour" + (remaining.getHours() !== 1 ? "s" : "") + " ";
+          }
+          if (remaining.getMinutes() !== 0) {
+            output += remaining.getMinutes() + " minute" + (remaining.getMinutes() !== 1 ? "s" : "") + " ";
+          }
+          if (remaining.getSeconds() !== 0) {
+            output += remaining.getSeconds() + " second" + (remaining.getSeconds() !== 1 ? "s" : " ");
+          }
+          embed.addField("Timer", output.trim(), false);
+        }
+
+        embed.addField("Looted", heister.heistStatWon[0] + " [" + heister.heistStatWon[1] + "]", true);
+        embed.addField("Apprehensions", heister.heistStatCaught[0] + " [" + heister.heistStatCaught[1] + "]", true);
+        embed.addField("Injuries", heister.heistStatInjured[0] + " [" + heister.heistStatInjured[1] + "]", true);
+        embed.addField("Deaths", heister.heistStatDead[0], true);
+        embed.addField("Credits Stolden", heister.heistStatStolden, true);
+
         message.channel.sendEmbed(embed);
 
       } else if (command.params[0] === "play") {
-        if (typeof data.data.user[message.author.id].heistStatus === "undefined") {
+        if (typeof data.data.user[message.author.id] === "undefined" || typeof data.data.user[message.author.id].heistStatus === "undefined") {
           message.channel.sendMessage("You're not part of the crew yet. Try joining them first.");
           return;
         }
@@ -87,11 +114,16 @@
         if (data.bin.heistState === 0) {
           data.bin.heistState = 1;
           data.bin.heistCrew = [message.member];
+          data.bin.heistLooters = [];
           message.react("\u2714"); // :heavy_check_mark:
-          message.channel.sendMessage("A heist is starting by " + grabServerName(message.member) + ".");
+          message.channel.sendMessage("A heist is starting by " + grabServerName(message.member) + ".").then(function(message) {
+            data.bin.heistJoinMessage = message;
+            message.react("\u2795"); // :heavy_plus_sign:
+          });
           setTimeout(function() {
 
             data.bin.heistState = 2;
+            data.bin.heistJoinMessage = null;
             var bank = null;
             for (var i = 0; i < data.data.heistBanks.length; i++) {
               if (data.data.heistBanks[i].size >= data.bin.heistCrew.length) {
@@ -105,9 +137,7 @@
               var user = data.data.user[data.bin.heistCrew[i].id];
               setTimeout(function(i) {
                 var action = null;
-                var ran = Math.random()
-                console.log(ran)
-                if (ran <= bank.difficulty) {
+                if (Math.random() <= bank.difficulty) {
                   do {
                     action = data.data.heistActions[Math.floor(Math.random() * data.data.heistActions.length)];
                   } while (action.result !== 0)
@@ -115,6 +145,22 @@
                   do {
                     action = data.data.heistActions[Math.floor(Math.random() * data.data.heistActions.length)];
                   } while (action.result === 0)
+                }
+
+                if (action.result === 0) {
+                  heister.heistStatWon[0]++;
+                  heister.heistStatWon[1]++;
+                } else if (action.result === 2) {
+                  heister.heistStatCaught[0]++;
+                  heister.heistStatCaught[1]++;
+                } else if ((action.result === 3 && user.heistStatus === 3) || action.result === 4) {
+                  heister.heistStatDead[0]++;
+                  heister.heistStatWon[1] = 0;
+                  heister.heistStatCaught[1] = 0;
+                  heister.heistStatInjured[1] = 0;
+                } else if (action.result === 3) {
+                  heister.heistStatInjured[0]++;
+                  heister.heistStatInjured[1]++;
                 }
 
                 var won = false;
@@ -126,10 +172,14 @@
                   user.heistStatus = 1;
                 } else if (user.heistStatus === 3 && action.result === 3) { // If injured, kill the user
                   user.heistStatus = 4;
-                } else if (user.heistStatus === 3 && action.result !== 4 && action.result !== 2) { // If injured, make sure the injury persists no matter the outcome
+                } else if (user.heistStatus === 3 && action.result !== 4 && action.result !== 2) { // If injured, make sure the injury persists
                   user.heistStatus = 3;
                 } else {
                   user.heistStatus = action.result;
+                }
+
+                if (won) {
+                  data.bin.heistLooters.push(data.bin.heistCrew[i]);
                 }
 
                 if (user.heistStatus === 4) {
@@ -141,7 +191,7 @@
                 message.channel.sendMessage("`" + action.text.replace("$name", grabServerName(data.bin.heistCrew[i])) + "`").then(function(message) {
                   var emote = "";
                   if (won) {
-                    message.react("\uD83D\uDCB0");
+                    message.react("\uD83D\uDCB0"); // :moneybag:
                   }
 
                   /*if (user.heistStatus === 1) {
@@ -149,11 +199,11 @@
                   } else*/
                   if (user.heistStatus !== 1) {
                     if (user.heistStatus === 2) {
-                      emote = "\uD83D\uDE94";
+                      emote = "\uD83D\uDE94"; // :oncoming_police_car:
                     } else if (user.heistStatus === 3) {
-                      emote = "\uD83D\uDC89";
+                      emote = "\uD83D\uDC89"; // :syringe:
                     } else if (user.heistStatus === 4) {
-                      emote = "\u2620";
+                      emote = "\u2620"; // :skull_crossbones:
                     }
                     message.react(emote);
                   }
@@ -164,9 +214,25 @@
             setTimeout(function() {
               message.channel.sendMessage("The heist has ended.");
               data.bin.heistState = 0;
+              setTimeout(function() {
+                if (data.bin.heistLooters.length > 0) {
+                  var creditPerLooter = Math.floor(bank.money / 2 / data.bin.heistLooters.length);
+                  var embed = new lib_discord.RichEmbed();
+                  embed.setTitle("Credits won");
+                  embed.setColor("#FFDF00");
+                  for (var i = 0; i < data.bin.heistLooters.length; i++) {
+                    var looter = data.data.user[data.bin.heistLooters[i].id];
+                    embed.addField(grabServerName(data.bin.heistLooters[i]), creditPerLooter);
+                    looter.money += creditPerLooter;
+                  }
+                  message.channel.sendEmbed(embed);
+                } else {
+                  message.channel.sendMessage("No one made it out safe.");
+                }
+              }, 2500);
             }, data.bin.heistCrew.length * 2500 + 5000);
 
-          }, 10 * 1000);
+          }, data.data.heistConfig.joinTimer);
         } else if (data.bin.heistState === 1) {
           for (var i = 0; i < data.bin.heistCrew.length; i++) {
             if (data.bin.heistCrew[i].id === message.author.id) {
@@ -181,7 +247,7 @@
         }
 
       } else if (command.params[0] === "release") {
-        if (typeof data.data.user[message.author.id].heistStatus === "undefined") {
+        if (typeof data.data.user[message.author.id] === "undefined" || typeof data.data.user[message.author.id].heistStatus === "undefined") {
           message.channel.sendMessage("You're not part of the crew yet. Try joining them first.");
           return;
         }
@@ -209,7 +275,7 @@
           message.channel.sendMessage("You've still have " + output + " left on your sentence.");
         }
       } else if (command.params[0] === "revive") {
-        if (typeof data.data.user[message.author.id].heistStatus === "undefined") {
+        if (typeof data.data.user[message.author.id] === "undefined" || typeof data.data.user[message.author.id].heistStatus === "undefined") {
           message.channel.sendMessage("You're not part of the crew yet. Try joining them first.");
           return;
         }
@@ -242,6 +308,27 @@
 
       } else {
         help();
+      }
+    },
+    reactionadd: function(reaction, client, data) {
+      if (data.bin.heistJoinMessage === null || data.bin.heistJoinMessage.author.id === reaction[1].id) {
+        return;
+      }
+
+      if (typeof data.data.user[reaction[1].id] === "undefined" || typeof data.data.user[reaction[1].id].heistStatus === "undefined") {
+        reaction[0].message.channel.sendMessage("You're not part of the crew yet. Try joining them first.");
+        return;
+      }
+
+      if (reaction[0].message.id === data.bin.heistJoinMessage.id && reaction[0].emoji.name === "\u2795") { // :heavy_plus_sign:
+        for (var i = 0; i < data.bin.heistCrew.length; i++) {
+          if (data.bin.heistCrew[i].id === reaction[1].id) {
+            return;
+          }
+        }
+
+        data.bin.heistCrew.push(reaction[0].message.guild.member(reaction[1]));
+        data.bin.heistJoinMessage.react("\u2714"); // :heavy_check_mark:
       }
     }
   }
