432858835efccc912a570fe395a1098ed0d191aa
Bit <BitNetwork@users.noreply.github.com>
2017-05-21 11:58:42 -0600

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let bot = new tilde();
 bot.addModification("core", require(path.join(__dirname, "modifications", "core.js")));
 bot.addModification("music", require(path.join(__dirname, "modifications", "music.js")), {dm: false});
 
-bot.login("MzE1Mjc5NjkzNTg0NzkzNjAw.DAEanQ.OUbdQxZzHqLA9SR-7CoHonekh54");
+bot.login("token");
 
 
 const readline = require("readline").createInterface({
